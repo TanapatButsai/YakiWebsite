5b6f44e4e9e5b3e299ef494d46aa69c2be8dd44e
Thanapat Butsai <thanapatbutsai@Thanapats-MacBook-Pro.local>
2023-09-24 17:44:00 +0700

    initialize commit

---
+++ src/main/java/ku/cs/YakinikuWebsite/YakinikuWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.YakinikuWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakinikuWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakinikuWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/YakinikuWebsite/YakinikuWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.YakinikuWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakinikuWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
