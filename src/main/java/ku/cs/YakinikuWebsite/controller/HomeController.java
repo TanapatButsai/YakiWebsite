7cf94fd739f6fa59a63ef507bb6f14a5fa7ac818
Thanapat Butsai <thanapatbutsai@Thanapats-MacBook-Pro.local>
2023-09-26 21:03:38 +0700

    1

---
+++ src/main/java/ku/cs/YakinikuWebsite/controller/HomeController.java
@@ -0,0 +1,17 @@
+package ku.cs.YakinikuWebsite.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "welcome to YakiWeb");
+        // ต้องคืนค่าเป็นชื่อไฟล์ html template โดยในเมธอดนี้ คืนค่าเป็น home.html
+        return "home";
+    }
+}
---
+++ src/main/java/ku/cs/YakinikuWebsite/entity/Employee.java
@@ -0,0 +1,24 @@
+package ku.cs.YakinikuWebsite.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+public class Employee {
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    private String username;
+    private String password;
+    private String name;
+    private String role;
+}
