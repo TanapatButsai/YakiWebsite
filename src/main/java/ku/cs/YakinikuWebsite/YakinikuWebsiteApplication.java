b83645513000aaacce077b2ed9f7f0996bc8d681
Tanapat Butsai <tanapat.bu@ku.th>
2023-10-07 03:30:50 +0700

    email sender service

--- src/main/java/ku/cs/YakinikuWebsite/YakinikuWebsiteApplication.java
+++ src/main/java/ku/cs/YakinikuWebsite/YakinikuWebsiteApplication.java
@@ -1,7 +1,13 @@
 package ku.cs.YakinikuWebsite;
 
+import ku.cs.YakinikuWebsite.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class YakinikuWebsiteApplication {
@@ -10,5 +16,15 @@ public class YakinikuWebsiteApplication {
 
 		SpringApplication.run(YakinikuWebsiteApplication.class, args);
 	}
-
+	// for email sending
+//	@Autowired
+//	private EmailSenderService senderService;
+//
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void triggerMail() throws MessagingException {
+//		senderService.sendSimpleEmail("tanapat.bu@ku.th",
+//				"This is email body",
+//				"This is email subject");
+//
+//	}
 }
