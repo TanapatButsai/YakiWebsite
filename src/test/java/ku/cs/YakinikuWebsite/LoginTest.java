2ceb7cf30b07ff1f4e352cf65d28cb1f01790c59
BallJedsadakorn <jedsadakorn.boo@ku.th>
2023-11-04 18:11:05 +0700

    test

---
+++ src/test/java/ku/cs/YakinikuWebsite/LoginTest.java
@@ -0,0 +1,69 @@
+package ku.cs.YakinikuWebsite;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+
+import java.time.Duration;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class LoginTest {
+
+
+
+    @LocalServerPort
+    private Integer port;
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driver.get("http://localhost:" + port + "/login");
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    @Test
+    void testLogin() {
+        WebElement usernameField = wait.until(webDriver ->
+                webDriver.findElement(By.id("inputUsername")));
+
+        WebElement passwordField = driver.findElement(By.id("inputPassword"));
+
+        WebElement submitButton = driver.findElement(By.id("submit-button"));
+
+
+        usernameField.sendKeys("ball1");
+
+        passwordField.sendKeys("123");
+
+        submitButton.click();
+
+    }
+
+}
---
+++ src/test/java/ku/cs/YakinikuWebsite/SignupTest.java
@@ -0,0 +1,74 @@
+package ku.cs.YakinikuWebsite;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+
+import java.time.Duration;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class SignupTest {
+
+
+    @LocalServerPort
+    private Integer port;
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driver.get("http://localhost:" + port + "/signup");
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    @Test
+    void testSignup() {
+        WebElement nameField = wait.until(webDriver ->
+                webDriver.findElement(By.id("inputName")));
+
+        WebElement usernameField = driver.findElement(By.id("inputUsername"));
+
+        WebElement emailField = driver.findElement(By.id("inputEmail"));
+
+        WebElement passwordField = driver.findElement(By.id("inputPassword"));
+
+        WebElement submitButton = driver.findElement(By.id("submit-button"));
+
+        nameField.sendKeys("BallJed");
+        usernameField.sendKeys("ball1");
+        emailField.sendKeys("ball9@gmail.com");
+        passwordField.sendKeys("123");
+
+        submitButton.click();
+
+    }
+
+
+}
+
