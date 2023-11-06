63b3056b4e8ab43e0b17160f4da1ef5a2d239102
TanapatBusai <tanapat.bu@ku.th>
2023-11-07 02:03:58 +0700

    fix test-02

--- src/test/java/ku/cs/YakinikuWebsite/SignupTest.java
+++ src/test/java/ku/cs/YakinikuWebsite/SignupTest.java
@@ -60,10 +60,16 @@ public class SignupTest {
 
         WebElement submitButton = driver.findElement(By.id("submit-button"));
 
+        WebElement addressField = driver.findElement(By.id("inputAddress"));
+
+        WebElement noteField = driver.findElement(By.id("inputNote"));
+
         nameField.sendKeys("BallJed");
         usernameField.sendKeys("ball1");
         emailField.sendKeys("ball9@gmail.com");
         passwordField.sendKeys("123");
+        passwordField.sendKeys("address01");
+        passwordField.sendKeys("note01");
 
         submitButton.click();
 
